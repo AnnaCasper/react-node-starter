3f62897723be3ca21189c881e5d7ffa75d156dac
Anna <anna@spiredigital.com>
2018-07-20 12:19:38 -0600

    more changes

--- server/models/index.js
+++ server/models/index.js
@@ -8,7 +8,14 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  	sequelize = new Sequelize(process.env[config.use_env_variable]);
+  	sequelize = new Sequelize(process.env[config.use_env_variable], {
+		dialect:  'postgres',
+		protocol: 'postgres',
+		logging:  true, //false,
+		dialectOptions: {
+			ssl: true
+		}
+	  });
 } else {
   	sequelize = new Sequelize(
     config.database, config.username, config.password, config
