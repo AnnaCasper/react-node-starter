e2a1bf722c7c06307b640fb95dbc39e9a308d473
Anna <anna@spiredigital.com>
2018-07-20 10:36:55 -0600

    add log

--- server/models/index.js
+++ server/models/index.js
@@ -9,6 +9,7 @@ const db = {};
 console.log(env, config, '===================')
 let sequelize;
 if (config.use_env_variable) {
+	console.log(config.use_env_variable)
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(
