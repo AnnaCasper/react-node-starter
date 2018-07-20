cb9ceeff765ab0c0d1c3956520068f271991456c
Anna <anna@spiredigital.com>
2018-07-20 09:27:08 -0600

    add cors middleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,29 @@
 const testController = require('../controllers').test;
 
 module.exports = (app) => {
-  app.get('/api', (req, res) => res.status(200).send({
-    message: 'Welcome to the React-Node-Starter API!',
-  }));
+	app.get('/api', (req, res) => res.status(200).send({
+		message: 'Welcome to the React-Node-Starter API!',
+	}));
 
-//   test
-  app.get('/api/test', testController.get);
+
+	// cors middleware
+	app.use((req, res, next) => {if (req.method === 'OPTIONS') {
+		var headers = {};
+		// IE8 does not allow domains to be specified, just the *
+		headers["Access-Control-Allow-Origin"] = "*";
+		headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+		headers["Access-Control-Allow-Credentials"] = false;
+		headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+		headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept, token";
+		res.writeHead(200, headers);
+		res.end();
+		} else {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+		next();
+		}
+	});
+
+	//  test
+	app.get('/api/test', testController.get);
 };
